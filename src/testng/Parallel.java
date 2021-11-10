b931e74d1c4b35ad1ab1e825f3b8461729647def
QSP <QSP@QSP>
2021-11-10 17:47:40 +0530

    TestNg-2

--- src/testng/Parallel.java
+++ src/testng/Parallel.java
@@ -5,50 +5,36 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Reporter;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
 
-public class Testcase1 {
+public class Parallel {
 	public WebDriver driver;
 	
-	@BeforeMethod
-	public void openApp() {
-	    driver=new ChromeDriver();
+	@Parameters({"browserName"})
+	@Test
+	public void demo(String browser) {
+		if(browser.equals("chrome")) {
+			driver=new ChromeDriver();
+		}
+		else
+		{
+			driver=new FirefoxDriver();
+		}
 		driver.manage().window().maximize();
 		driver.get("https://www.facebook.com/");
 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-		
-	}
-	
-	@Test
-	public void tc1() {
 		driver.findElement(By.id("email")).sendKeys("tyghgyutggyugx");
 		driver.findElement(By.id("pass")).sendKeys("hgfjghghfgs");
 		driver.findElement(By.name("login")).click();
 		String title = driver.getTitle();
-		
-		//Assert
-		Assert.assertEquals(title,"54554rt65dre4");
-		
-		//softassert
-		SoftAssert a=new SoftAssert();
-		a.assertEquals(title,"Facebook – log in or sign up");
-		
 		Reporter.log(title,true);
 		String url = driver.getCurrentUrl();
 		Reporter.log(url,true);
 		
-		a.assertAll();
 		
-	}
-	
-	@AfterMethod
-	public void closeapp() {
-		driver.close();
 	}
 
 }
